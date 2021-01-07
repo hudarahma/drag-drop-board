e3fd04a6296cca1a182e800ea48cb84cd4f69c46
Orkid <mrs.emadi@yahoo.com>
2021-01-07 21:39:57 +0330

    finishing app

--- script.js
+++ script.js
@@ -171,27 +171,12 @@ function hideInputBox(column) {
 
 // Allow arrays to reflect Drag and Drop items
 function rebuildArrays() {
-  console.log(backlogList.children);
-  console.log(progressList.children);
-  backlogListArray = [];
-  progressListArray = [];
-  completeListArray =[];
-  onHoldListArray= [];
-  for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i].textContent);
-  }
-
-  for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i].textContent);
-  }
-
-  for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i].textContent);
-  }
-
-  for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i].textContent);
-  }
+  // console.log(backlogList.children);
+  // console.log(progressList.children);
+  backlogListArray = Array.from(backlogList.children).map(i => i.textContent);
+  progressListArray =  Array.from(progressList.children).map(i => i.textContent);;
+  completeListArray = Array.form(completeList.children).map(i => i.textContent);
+  onHoldListArray = Array.from(onHoldList.children).map(i => i.textContent);
 
   updateDOM();
 }
